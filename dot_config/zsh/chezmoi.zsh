f30cbeb44ccae2c020a7e9968d0a9dbdb733bebd
Nils Müller <tyriis@users.noreply.github.com>
2023-04-09 23:40:42 +0200

    feat(zsh): chezmoi autocompletion not working, keep it for now

--- dot_config/zsh/chezmoi.zsh
+++ dot_config/zsh/chezmoi.zsh
@@ -1,3 +1,4 @@
-if which chezmoi &>/dev/null; then
-    source "$(dirname $(which chezmoi))/../share/zsh/site-functions/_chezmoi"
-fi
+# if which chezmoi &>/dev/null; then
+#     echo "load chezmoi"
+#     echo "$(dirname $(which chezmoi))/../share/zsh/site-functions/_chezmoi"
+# fi
