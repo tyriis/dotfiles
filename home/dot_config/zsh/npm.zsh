ef434e51233d4e0df1ed24cad7a3584d64661fb2
Nils Müller <tyriis@users.noreply.github.com>
2024-03-11 00:55:30 +0100

    feat(nixos): adjust config to work with nix

--- home/dot_config/zsh/npm.zsh
+++ home/dot_config/zsh/npm.zsh
@@ -4,17 +4,20 @@ export PATH=$HOME/.npm-global/bin/:$PATH
 
 export NPM_PACKAGES=$HOME/.npm
 
-# init nvm
-source /usr/share/nvm/init-nvm.sh
+# only load when available
+if [[ -d /usr/share/nvm && -f /usr/share/nvm/init-nvm.sh ]]; then
+  # init nvm
+  source /usr/share/nvm/init-nvm.sh
 
-autoload -U add-zsh-hook
-load-nvmrc() {
-  if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use --silence
-  elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    # echo "Reverting to nvm default version"
-    nvm use default --silence
-  fi
-}
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+  autoload -U add-zsh-hook
+  load-nvmrc() {
+    if [[ -f .nvmrc && -r .nvmrc ]]; then
+      nvm use --silence
+    elif [[ $(nvm version) != $(nvm version default)  ]]; then
+      # echo "Reverting to nvm default version"
+      nvm use default --silence
+    fi
+  }
+  add-zsh-hook chpwd load-nvmrc
+  load-nvmrc
+fi
